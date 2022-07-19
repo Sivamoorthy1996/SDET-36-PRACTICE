14af574de055cc69920909b82e8ef912a1b2200f
Dinesh.M <Dinesh.M@LAPTOP-8CCMCL84>
2022-07-19 14:30:00 +0530

    my 1st commit for github practice

---
+++ GITHUBPRACTICE/src/test/java/org/github/practice/githubpractice1Test.java
@@ -0,0 +1,28 @@
+package org.github.practice;
+
+import org.testng.annotations.Test;
+
+public class githubpractice1Test {
+	
+	@Test
+	public void parctice1() {
+		
+		System.out.println("test1==> class1");
+	
+	}
+	@Test
+	public void parctice2() {
+		
+		System.out.println("Test1-->class1");
+		System.out.println("Good boy");
+	
+	}
+	@Test
+	public void parctice3() {
+		System.out.println("Test1-->class1");
+	
+	}
+	
+	
+
+}
